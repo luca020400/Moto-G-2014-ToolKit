ab676a372b99782b261764ce7f32234e47911755
luca020400 <luca.stefani.ge1@gmail.com>
2014-12-21 14:41:37 +0100

    Fix root.bat

--- root.bat
+++ root.bat
@@ -1,6 +1,24 @@
 @echo off
-if %1==titan_retaildsds bin\fastboot.exe boot autoroot/titan_retaildsds.img
-if %1==titan_retde bin\fastboot.exe boot autoroot/titan_retde.img
-if %1==titan_retuglb bin\fastboot.exe boot autoroot/titan_retuglb.img
-if %1==titan_* echo "Unsupported Device"
-else echo "You have to type root.bat device ( titan_ret*** )"
+if %1==titan_retaildsds goto titan_retaildsds 
+if %1==titan_retde goto titan_retde
+if %1==titan_retuglb goto titan_retuglb
+
+echo "You have to type root.bat device ( titan_ret*** )"
+goto :exit
+
+:titan_retaildsds
+bin\fastboot.exe boot autoroot/titan_retaildsds.img
+goto exit
+
+:titan_retde
+bin\fastboot.exe boot autoroot/titan_retde.img
+goto exit
+
+:titan_retuglb
+bin\fastboot.exe boot autoroot/titan_retuglb.img
+goto exit
+
+:unsupported
+echo "Unsupported Device"
+
+:exit
