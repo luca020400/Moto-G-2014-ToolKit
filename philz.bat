99a705a310db01a41f47dfcde1ec6a9e72d11965
luca020400 <luca.stefani.ge1@gmail.com>
2014-12-21 14:34:40 +0100

    Fix philz/twrp.bat
    F**k batch

--- philz.bat
+++ philz.bat
@@ -1,4 +1,15 @@
 @echo off
-if %1==boot bin\fastboot.exe boot img\philz.img
-if %1==flash bin\fastboot.exe flash recovery img\philz.img
-else echo "You have to type philz.bat flash or boot"
+if %1==boot goto fai_boot
+if %1==flash goto fai_flash
+
+echo "You have to type philz.bat flash or boot"
+goto esci
+
+:fai_boot
+bin\fastboot.exe boot img\philz.img
+goto esci
+
+:fai_flash
+bin\fastboot.exe flash recovery img\philz.img
+
+:esci
--- twrp.bat
+++ twrp.bat
@@ -1,4 +1,15 @@
 @echo off
-if %1==boot bin\fastboot.exe boot img\twrp.img
-if %1==flash bin\fastboot.exe flash recovery img\twrp.img
-else echo "You have to type twrp.bat flash or boot"
+if %1==boot goto fai_boot
+if %1==flash goto fai_flash
+
+echo "You have to type twrp.bat flash or boot"
+goto esci
+
+:fai_boot
+bin\fastboot.exe boot img\twrp.img
+goto esci
+
+:fai_flash
+bin\fastboot.exe flash recovery img\twrp.img
+
+:esci
