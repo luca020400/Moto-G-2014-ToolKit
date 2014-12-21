36dcc6b0e7383313853f2a66ac2b99ae1cf5f382
luca020400 <luca.stefani.ge1@gmail.com>
2014-12-21 19:43:50 +0100

    Attempt to fix bootloader relock

--- bootloader.sh
+++ bootloader.sh
@@ -21,6 +21,10 @@ case "$1" in
             ;;
 
         relock)
+	    $fastboot lock begin
+	    echo "Now you have to flash the stock image"
+	    echo "This is an example http://forum.xda-developers.com/moto-g-2014/general/restore-to-stock-t2873657"
+ 	    echo "When done press Enter"; read line
             $fastboot oem lock
 	    echo "Your bootloader is now locked"
             ;;
