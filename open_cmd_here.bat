b063f3348eb47ea76a3b6b6ffd1291bc1cffddbd
Luca Stefani <luca.stefani.ge1@gmail.com>
2015-02-18 13:48:09 +0100

    FixUp for windows

--- open_cmd_here.bat
+++
@@ -1,2 +0,0 @@
-cd /d %~dp0
-cmd.exe /s /k "pushd ""%V"" & title %V"
--- toolkit.bat
+++ toolkit.bat
@@ -2,7 +2,6 @@
 
 set adb="bin\adb.exe"
 set fastboot="bin\fastboot.exe"
-set log_file=".log_toolkit"
 set supersuver="2.46"
 set supersuzip="UPDATE-SuperSU-v$supersuver.zip"
 set twrp="img\openrecovery-twrp-2.8.5.0-titan.img"
@@ -12,56 +11,57 @@ goto disclaimer
 goto menu
 
 :toolkit
-echo "Universal Moto G 2014 Toolkit"
-echo "By luca020400"
-echo "Version 2.0"
-echo 
-echo "Press Enter to continue"
-echo "Press CTRL+c to abort"
+echo Universal Moto G 2014 Toolkit
+echo By luca020400
+echo Version 2.0
+echo
+echo Press Enter to continue
+echo Press CTRL+c to abort
 pause > nul
 
 :disclaimer
-echo "/*"
-echo  "* Your warranty is now void. Knox 0x1."
-echo  "*"
-echo  "* I am not responsible for bricked devices, dead SD cards,"
-echo  "* thermonuclear war, or you getting fired because the alarm app failed."
-echo  "* Please do some research if you have any concerns about this tool !"
-echo  "* YOU are choosing to make these modifications, and if"
-echo  "* you point the finger at me for messing up your device, I will laugh at you. Hard. A lot."
-echo  "*/"
+echo /*
+echo  * Your warranty is now void. Knox 0x1.
+echo  *
+echo  * I am not responsible for bricked devices, dead SD cards,
+echo  * thermonuclear war, or you getting fired because the alarm app failed.
+echo  * Please do some research if you have any concerns about this tool !
+echo  * YOU are choosing to make these modifications, and if
+echo  * you point the finger at me for messing up your device, I will laugh at you. Hard. A lot.
+echo  */
 
 :adb_authorization
-echo "You have to enable USB Debugging in Developer Settings"
-echo "When done Press Enter"
+echo You have to enable USB Debugging in Developer Settings
+echo When done Press Enter
 pause > nul
-echo "Click 'Always allow from this computer'"
-echo "And then OK"
-echo "When done Press Enter"
-%adb% wait-for-device
+echo Click 'Always allow from this computer'
+echo And then OK
+echo When done Press Enter
+%adb% wait-for-device > nul
 
 :menu
-echo "Universal Moto G 2014 Toolkit Menu:"
-echo "[rb] Reboot to Bootloader from Phone"
-echo "[rp] Reboot to Phone from Bootloader"
-echo "[rr] Reboot to Recovery from Phone"
-echo "[1] TWRP 2.8.5.0 Flash"
-echo "[2] TWRP 2.8.5.0 Boot"
-echo "[3] Philz Flash"
-echo "[4] Philz Boot"
-echo "[5] Root with SuperSU %supersuver%"
-echo "[6] Logo Without Warning"
-echo "[7] Logo With Warning"
-echo "[8] Busybox Installer"
-echo "[9] Bootloader Unlock"
-echo "[10] Bootloader Relock"
-echo "[q] Exit"
-echo ""
-SET /P choice="> "
-echo ""
-if %choice%==rb %adb% reboot-bootloader & cls
-if %choice%==rp %fastboot% reboot & cls
-if %choice%==rr %adb% reboot & cls
+echo Universal Moto G 2014 Toolkit Menu
+echo
+echo [rb] Reboot to Bootloader from Phone
+echo [rp] Reboot to Phone from Bootloader
+echo [rr] Reboot to Recovery from Phone
+echo [1] TWRP 2.8.5.0 Flash
+echo [2] TWRP 2.8.5.0 Boot
+echo [3] Philz Flash
+echo [4] Philz Boot
+echo [5] Root with SuperSU %supersuver%
+echo [6] Logo Without Warning
+echo [7] Logo With Warning
+echo [8] Busybox Installer
+echo [9] Bootloader Unlock
+echo [10] Bootloader Relock
+echo [q] Exit
+echo
+SET /P choice="> 
+echo
+if %choice%==rb %adb% reboot-bootloader > nul & cls
+if %choice%==rp %fastboot% reboot > nul & cls
+if %choice%==rr %adb% reboot > nul & cls
 if %choice%==1 goto twrp_flash
 if %choice%==2 goto twrp_boot
 if %choice%==3 goto philz_flash
@@ -73,7 +73,7 @@ if %choice%==8 goto busybox
 if %choice%==9 goto bootloader_unlock
 if %choice%==10 goto bootloader_relock
 if %choice%==q exit /b
-else echo "Error Unknown Command" & cls & goto :menu
+else echo Error Unknown Command & cls & goto :menu
 
 :twrp_flash
 %fastboot% flash recovery %twrp%
@@ -124,18 +124,18 @@ cls
 goto :menu
 
 :bootloader_unlock
-echo "Are you sure ?? Do you know the risks ?? Are you willing to do this ??"
-echo "If so Press Enter"
+echo Are you sure ?? Do you know the risks ?? Are you willing to do this ??
+echo If so Press Enter
 pause > nul
-echo "Go to http://bit.ly/UpVtsa and read the risks"
-ping 1.1.1.1 -n 1 -w 2000 > nul
-echo "When done press Enter"
+echo Go to http://bit.ly/UpVtsa and read the risks
+ping 1.1.1.1 -n 1 -w 3000 > nul
+echo When done press Enter
 pause > nul
-echo "Follow the guide inside the site and then"
-echo "Enter this code"
+echo Follow the guide inside the site and then
+echo Enter this code
 %fastboot% oem get_unlock_data
-echo "As shown in the example"
-echo "Enter the key emailed to you here :"
+echo As shown in the example
+echo Enter the key emailed to you here
 SET /P code="> "
 %fastboot% oem unlock $code
 cls
@@ -143,11 +143,11 @@ goto :menu
 
 :bootloader_relock
 %fastboot% lock begin
-echo "Now you have to flash the stock image"
-echo "This is an example http://forum.xda-developers.com/moto-g-2014/general/restore-to-stock-t2873657"
-echo "When done press Enter"
+echo Now you have to flash the stock image
+echo This is an example http://forum.xda-developers.com/moto-g-2014/general/restore-to-stock-t2873657
+echo When done press Enter
 pause > nul
 %fastboot% oem lock
-echo "Your bootloader is now locked"
+echo Your bootloader is now locked
 cls
 goto :menu
